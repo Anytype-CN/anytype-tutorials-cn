9fd1d2f74b1fdaa06d4f6985b4565ae5cc7feab4
Calvin.G <ScreeperC@outlook.com>
2023-08-18 21:04:27 +0800

    Init Commit and Add frame work

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Re-builds the full page
+
+# First retrieve all available docs
+cd "$(dirname $0)"
+cd ..
+cd ./docs
+docs=$(ls -d *)
+cd .. # Switch back to root directory
+
+for d in $docs
+do
+  # Build each available language
+  mkdocs build --config-file ./config/$d.yml
+done
+
+# Add the index-file with language selection
+cp ./assets/index.html ./build
+# ... as well as the logo
+# cp ./assets/logo.png ./build
